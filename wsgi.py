6a9d22da60e92c1203faf2fe2880b859ffbfae82
Serzas P <slipik@inbox.ru>
2018-10-12 16:25:42 +0300

    Initial commit with changes to display room-availability graphics

--- wsgi.py
+++ wsgi.py
@@ -8,11 +8,17 @@ import json
 
 application = Flask(__name__)
 config = { "floors": {4: { "ec:fa:bc:e:a6:95_1": "general" , "ec:fa:bc:e:a6:95_2": "general", "ec:fa:bc:e:a6:95_3": "glass", "ec:fa:bc:e:a6:95_4": "paper" }}}
+ra_config = { "floors": {4: { "b8:27:eb:54:2c:38_1": "general" , "ec:fa:bc:e:a6:95_2": "general", "ec:fa:bc:e:a6:95_3": "glass", "ec:fa:bc:e:a6:95_4": "paper" }}}
 
 @application.route("/")
 def index():	
     templateData = get_template_data()
     return render_template('index.html', **templateData)
+
+@application.route("/ra")
+def ra_index():
+    templateData = get_ra_template_data()
+    return render_template('ra_index.html', **templateData)
 
 @application.route('/date/', methods=['POST'])
 def get_graph_data():
@@ -33,6 +39,36 @@ def get_graph_data():
 @application.route('/static/<path:path>')
 def send_ota(path):
     return send_from_directory('static', path)
+
+def get_ra_template_data(date=None):
+    templateData = {}
+    templateData['config'] = config
+    user = "remote-admin"
+    passwd = "Some-pass!23"
+    dbhost = os.environ["MYSQL_SERVICE_HOST"]
+    dbname = "room-availability"
+    db = MySQLdb.connect(host=dbhost, user=user, passwd=passwd, db=dbname)
+    cur = db.cursor()
+#        cur.execute("""SELECT * FROM `sensor_data` WHERE timestamp < '2018-08-06 09:38:40' ORDER BY timestamp DESC LIMIT 200""")
+    if date is None:
+        mysql_string = "SELECT * FROM `sensor_data` ORDER BY timestamp DESC LIMIT 200"
+    else:
+        mysql_string = "SELECT * FROM `sensor_data` WHERE timestamp > '{}' ORDER BY timestamp DESC".format(date)
+    cur.execute(mysql_string)
+    data = cur.fetchall()
+    parsed_data = OrderedDict()
+    for (id, mac_id, distance, datetime_object) in data:
+        date_time = datetime_object.strftime("%Y-%m-%d %H:%M:%S")
+        if date_time in parsed_data:
+            parsed_data[date_time].update({ mac_id: { "busyness": data }})
+        else:
+            parsed_data.update({ date_time: { mac_id: { "busyness": data }}})
+    filtered_data = OrderedDict()
+    for i in parsed_data:
+        if len(parsed_data[i]) == 4:
+            filtered_data.update({i: parsed_data[i]})
+    templateData['busyness_data'] = OrderedDict(sorted(filtered_data.items()))
+    return templateData
 
 def get_template_data(date=None):
     templateData = {}
