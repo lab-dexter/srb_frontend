6af31819ef56716f170d1626c7e0e5b2f6eb35cf
KipsasJaujoj <vaidotasslavinskas@gmail.com>
2019-03-26 14:09:26 +0200

    sorting the tuple and reversing the order

--- wsgi.py
+++ wsgi.py
@@ -156,7 +156,7 @@ def db_records():
     mysql_string = "SELECT * FROM `sensor_data` ORDER BY timestamp DESC LIMIT 100"
     cur.execute(mysql_string)
     data = cur.fetchall()
-    data.sort(key=lambda tup: tup[1], reverse=True)
+    data.sort(key=lambda tup: tup[1])
     return render_template('db.html', data=data)
 
 
