7f16ba77043a524229a1d9e35e7f4221093ce6b9
KipsasJaujoj <vaidotasslavinskas@gmail.com>
2019-03-26 13:44:54 +0200

    troubleshooting

--- wsgi.py
+++ wsgi.py
@@ -156,7 +156,7 @@ def db_records():
     mysql_string = "SELECT * FROM `sensor_data` ORDER BY timestamp DESC LIMIT 100"
     cur.execute(mysql_string)
     data = cur.fetchall()
-    return render_template('db.html')
+    return render_template('db.html', data=data)
 
 
 if __name__ == "__main__":
