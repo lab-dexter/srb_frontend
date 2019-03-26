a15251471b23457cf394bf5d694f65c115880dfc
KipsasJaujoj <vaidotasslavinskas@gmail.com>
2019-03-26 14:22:36 +0200

    sorting by id instead of time while querying mysql

--- wsgi.py
+++ wsgi.py
@@ -154,7 +154,7 @@ def db_records():
     db_name = "smart-recycling-bins"
     db = MySQLdb.connect(host=db_host, user=user, passwd=passwd, db=db_name)
     cur = db.cursor()
-    mysql_string = "SELECT * FROM `sensor_data` ORDER BY timestamp DESC LIMIT 100"
+    mysql_string = "SELECT * FROM `sensor_data` ORDER BY id timestamp DESC LIMIT 100"
     cur.execute(mysql_string)
     data = list(cur.fetchall())
     data.sort(key=itemgetter(1), reverse=True)
